e55e5bc32edfc2387ddf0d927d966ed4351295ac
Mark <marke9135@sd61learn.ca>
2022-04-13 22:40:33 -0700

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,10 +1,23 @@
 from flask import Flask
 from threading import Thread
+import requests
+from bs4 import BeautifulSoup
+import cloudscraper
+
+def makeRequest():
+
+    url = "http://hypixel.net/threads/hypixel-mafia-halloween-in-january-town-win-game-xlviii.3770594/"
+    scraper = cloudscraper.create_scraper()
+    response = scraper.get(url).text
+    soup = BeautifulSoup(response, 'html.parser')
+    print(soup.text)
+    return
 
 app = Flask('')
 
 @app.route('/')
 def home():
+    makeRequest()
     return "I'm alive"
 
 def run():
