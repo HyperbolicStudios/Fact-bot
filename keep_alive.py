824b57f0dc007facf2bd22eb25aa719dd3a85419
Mark <marke9135@sd61learn.ca>
2022-04-16 11:40:30 -0700

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -8,7 +8,8 @@ def makeRequest():
 
     url = "http://hypixel.net/threads/hypixel-mafia-halloween-in-january-town-win-game-xlviii.3770594/"
     scraper = cloudscraper.create_scraper()
-    response = scraper.get(url).text
+    response = scraper.get(url,headers={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36"}).text
+
     soup = BeautifulSoup(response, 'html.parser')
     print(soup.text)
     return
